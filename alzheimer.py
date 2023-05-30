a2588fea91e1a826a651ebacab5826bed83edcf9
Ayşegül <81236984+aysegultoptas@users.noreply.github.com>
2023-05-30 23:29:40 +0300

    Create alzheimer.py

---
+++ alzheimer.py
@@ -0,0 +1,130 @@
+import tensorflow as tf
+from tensorflow import keras 
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+from keras.utils import img_to_array
+import keras.utils as image
+
+import cv2
+import os
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D,Input
+
+base_dir =r'C:/Users/ayseg/archive/Alzheimer_s Dataset/'
+
+train = ImageDataGenerator(rescale=1./255)
+test =  ImageDataGenerator(rescale=1./255)
+val =  ImageDataGenerator(rescale=1./255)
+
+train=base_dir+ 'train/'
+
+train_data = keras.preprocessing.image_dataset_from_directory(
+    train,
+    #validation_split=0.2,
+    image_size=(224,224),
+    batch_size=32,
+    #subset='training',
+    seed=1000 
+    )
+
+val=base_dir+ 'train/'
+
+val_data = keras.preprocessing.image_dataset_from_directory(
+    val,
+    validation_split=0.2,
+    image_size=(224,224),
+    batch_size=32,
+    subset='validation',
+    seed=1000
+    )
+
+test=base_dir +'test/'
+
+test_data = keras.preprocessing.image_dataset_from_directory(
+    test,
+    image_size=(224,224),
+    batch_size=32,
+    seed=1000
+    )
+
+class_names = ['MildDementia', 'ModerateDementia', 'NonDementia', 'VeryMildDementia']
+
+train_data.class_names = class_names
+val_data.class_names = class_names
+
+
+model=Sequential()
+
+model.add(Conv2D(16,(3,3), activation='relu', input_shape=(224,224,3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32,(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64,(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128,(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Conv2D(256,(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(512,activation='relu'))
+model.add(Dense(256,activation='relu'))
+model.add(Dropout(0.7))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(4,activation='softmax'))
+
+
+model.summary()
+
+model.compile(optimizer = tf.keras.optimizers.Adam(1e-4), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+history = model.fit(train_data, validation_data=val_data, epochs=50)
+
+model.evaluate(test_data)
+
+
+loss_train = history.history['loss']
+loss_val = history.history['val_loss']
+plt.plot(loss_train, 'g', label='Training loss')
+plt.plot(loss_val, 'b', label='validation loss')
+plt.title('Training and Validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig("train_vs_val_loss.png")
+plt.show()
+
+accuracy_train = history.history['accuracy']
+accuracy_val = history.history['val_accuracy']
+plt.plot(accuracy_train, 'g', label='Training accuracy')
+plt.plot(accuracy_val, 'b', label='Validation accuracy')
+plt.title('Training and Validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig("train_vs_val_acc.png")
+plt.show()
+
+class_names={0:"Mild Dementia", 1:"Moderate Dementia", 2:"Non Dementia", 3:"Very Mild Dementia"}
+
+plt.figure(figsize=(18,8))
+for images, labels in val_data.take(1):
+    for i in range(10):
+        plt.subplot(2,5,i+1)
+        plt.imshow(images[i]/255)
+        frame1 = plt.gca()
+        frame1.axes.xaxis.set_ticklabels([])
+        frame1.axes.yaxis.set_ticklabels([])
+        x = img_to_array(images[i])
+        x = np.expand_dims(x, axis=0)
+        p=np.argmax(model.predict(x))    
+        plt.title("Predicted: {}".format(class_names[p]))
+        plt.xlabel("True Class: {}".format(val_data.class_names[labels[i]]))    
+plt.show()
